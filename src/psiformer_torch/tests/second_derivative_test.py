e1f27c4fc7a815a5e3fa3ff0d7dfef7b4beede3c
jorgemunozl <alvaro18ml@gmail.com>
2025-12-12 20:56:54 -0500

    Testing custom op for backward

---
+++ src/psiformer_torch/tests/second_derivative_test.py
@@ -0,0 +1,50 @@
+"""
+Test of determinant first and second derivative using implementation from
+backwards.py
+"""
+import torch
+from psiformer_torch.backwards import DetWithCofactor
+
+
+def f1(X: torch.Tensor) -> torch.Tensor:
+    return X*2
+
+
+def function(A: torch.Tensor) -> torch.Tensor:
+    return torch.det(A)
+
+
+def my_function(A: torch.Tensor):
+    # A = f1(A)
+    return DetWithCofactor.apply(A)
+
+
+def first_derivative(func, x):
+    """
+    Test for the first derivative
+    """
+    if x.grad is not None:
+        x.grad.zero_()
+    Y = func(x)
+    Y.backward()
+    print("Derivate of Y wrt X:\n", x.grad)
+
+
+def second_derivative(func, x):
+    """
+    Test for the second derivative
+    """
+
+
+if __name__ == "__main__":
+
+    # If X is a singular function then the derivative becomes zero.
+    # If matriz singular then it doesn't try to compute the gradients
+    X_singular = torch.tensor([[1.0, 2.0],
+                              [2.0, 4.0]], requires_grad=True)
+
+    X_n_singular = torch.tensor([[3.0, 2.0],
+                                [2.0, 2.0]], requires_grad=True)
+
+    # dot = make_dot(Y, params={"X": X})
+    # dot.render("det_trace_graph", format="png", cleanup=True)
